46a29d2caa49c4076cff2d6df3db067297506d0b
annz-gif <96661837+annz-gif@users.noreply.github.com>
2024-02-13 21:51:36 +0530

    Update dsa16.cpp

---
+++ dsa16.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int i,ans=1,m=INT_MIN;
+        for(i=0;i<nums.size();i++)
+        {
+            ans=ans*nums[i];
+            
+            if(m<ans)
+            m=ans;
+            if(ans==0) 
+            ans=1;
+        }
+        ans=1;
+        for(i=nums.size()-1;i>=0;i--)
+        {
+            ans=ans*nums[i];
+            if(m<ans)
+            m=ans;
+            if(ans==0)
+            ans=1;
+        }
+        return m;
+    }
